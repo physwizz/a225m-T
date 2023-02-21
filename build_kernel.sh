83d0a634175544d4ddac43a809a5dd570f542554
physwizz <pollockg86@gmail.com>
2023-02-22 07:22:39 +1100

    fix build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+make clean && make mrproper
 export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
